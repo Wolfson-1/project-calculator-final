4685f5e7060abfb946aecd90c07de7f3081756ad
wolfson-1 <harrywolverson@googlemail.com>
2022-10-30 21:18:56 +0000

    LINK NUM & DEL BUTTONS TO DISPLAY NUMBER: Link each numerical button to add to the display variable. Also link clear & delete keys todisplay variable. Display dispVariable in html calculator via the DOM.

--- calc-script.js
+++ calc-script.js
@@ -1,3 +1,30 @@
+//vairables
+let dispValue = "";
+//number button query selectors
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eight");
+const nineBtn = document.querySelector(".nine");
+const zeroBtn = document.querySelector(".zero");
+
+//operator button query selectors
+const addBtn = document.querySelector(".plus");
+const subBtn = document.querySelector(".minus");
+const divideBtn = document.querySelector(".divide");
+const multiBtn = document.querySelector(".multi");
+const equalBtn = document.querySelector(".equal");
+const deciBtn = document.querySelector(".deci");
+const clearBtn = document.querySelector(".clear");
+const delBtn = document.querySelector(".delete");
+
+//display query selector
+document.getElementById("display").innerHTML = dispValue;
+
 // addition funciton
 function add (a,b) {
 return a + b;
@@ -30,3 +57,97 @@ switch (operator) {
     case "*":
         return multiply(a,b);
 }};
+
+//Numeric button event listeners
+oneBtn.addEventListener('click', () => {
+    dispValue += "1";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+twoBtn.addEventListener('click', () => {
+    dispValue += "2";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+threeBtn.addEventListener('click', () => {
+    dispValue += "3";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+fourBtn.addEventListener('click', () => {
+    dispValue += "4";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+fiveBtn.addEventListener('click', () => {
+    dispValue += "5";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+sixBtn.addEventListener('click', () => {
+    dispValue += "6";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+sevenBtn.addEventListener('click', () => {
+    dispValue += "7";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+eightBtn.addEventListener('click', () => {
+    dispValue += "8";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+nineBtn.addEventListener('click', () => {
+    dispValue += "9";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+zeroBtn.addEventListener('click', () => {
+    dispValue += "0";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+deciBtn.addEventListener('click', () => {
+    dispValue += ".";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+//clear event listener
+clearBtn.addEventListener('click', () => {
+    dispValue = "";
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
+
+//delete event listener
+delBtn.addEventListener('click', () => {
+   dispValue = dispValue.slice(0, -1);
+
+    //display query selector
+    document.getElementById("display").innerHTML = dispValue;
+});
